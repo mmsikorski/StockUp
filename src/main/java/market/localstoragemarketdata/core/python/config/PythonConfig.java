602e6e04d195c9669b7ce0c8970331a056de8515
mmsikorski <mm.sikorski3@gmail.com>
2023-03-11 15:47:17 +0100

    feat: refactor code

--- src/main/java/market/localstoragemarketdata/core/python/config/PythonConfig.java
+++ src/main/java/market/localstoragemarketdata/core/python/config/PythonConfig.java
@@ -1,5 +1,7 @@
-package market.localstoragemarketdata.core.python;
+package market.localstoragemarketdata.core.python.config;
 
+import market.localstoragemarketdata.core.python.CommandPython;
+import market.localstoragemarketdata.core.python.PythonProcessRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
