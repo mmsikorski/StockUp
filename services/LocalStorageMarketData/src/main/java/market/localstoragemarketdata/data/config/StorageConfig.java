b8498cebe20ac78a2b12b998ee845c92f39dc4b6
mmsikorski <mm.sikorski3@gmail.com>
2023-03-11 15:59:58 +0100

    feat: files relocation again

--- services/LocalStorageMarketData/src/main/java/market/localstoragemarketdata/data/config/StorageConfig.java
+++ services/LocalStorageMarketData/src/main/java/market/localstoragemarketdata/data/config/StorageConfig.java
@@ -2,7 +2,6 @@ package market.localstoragemarketdata.data.config;
 
 import lombok.extern.slf4j.Slf4j;
 import market.localstoragemarketdata.data.StorageRunner;
-import market.localstoragemarketdata.data.localfile.CsvPricesExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
