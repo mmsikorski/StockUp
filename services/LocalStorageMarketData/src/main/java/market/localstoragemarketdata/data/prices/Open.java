cdb015c46fd8d80a13ad689b73e4bebf56ea9412
fibonafide <maciej.setorek@gmail.com>
2023-03-14 22:22:37 +0100

    remove unnecessary price setter

--- services/LocalStorageMarketData/src/main/java/market/localstoragemarketdata/data/prices/Open.java
+++ services/LocalStorageMarketData/src/main/java/market/localstoragemarketdata/data/prices/Open.java
@@ -6,8 +6,4 @@ import market.localstoragemarketdata.data.Price;
 @Data
 public class Open extends AbstractPrice implements Price {
 
-    @Override
-    public void setPrice(double price) {
-        this.price = price;
-    }
 }
